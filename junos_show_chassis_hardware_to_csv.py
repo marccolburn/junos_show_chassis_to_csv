52e466f8d9f5e231e4779ec809846690c5db3a91
Marc Colburn <mcolburn@juniper.net>
2023-02-09 19:55:25 -0500

    Included Brandon's changes for file header on new file creation as well as additional column for part to parent correlation

--- junos_show_chassis_hardware_to_csv.py
+++ junos_show_chassis_hardware_to_csv.py
@@ -12,22 +12,31 @@ def output_to_json(json_raw_data):
 def dump_json_to_csv(json_data):
     """Take JSON data and dump part descriptions with serial number in a csv"""
     chassis_modules = json_data["chassis-inventory"][0]["chassis"][0]["chassis-module"]
-    with open('hardware_parts.csv', mode='a') as hardware_file:
+    output_file = "hardware_parts.csv"
+    if not os.path.exists(output_file):
+        with open(output_file, mode='x') as hardware_file:
+            schema = ['Name', 'Description', 'DeviceSN','ParentSN']
+            writer = csv.writer(hardware_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([g for g in schema])
+    with open(output_file, mode='a') as hardware_file:
         hardware_writer = csv.writer(hardware_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         hardware_writer.writerow(
             [
                 json_data["chassis-inventory"][0]["chassis"][0]["name"][0]["data"], 
                 json_data["chassis-inventory"][0]["chassis"][0]["description"][0]["data"], 
+                json_data["chassis-inventory"][0]["chassis"][0]["serial-number"][0]["data"],
                 json_data["chassis-inventory"][0]["chassis"][0]["serial-number"][0]["data"]
             ]
         )
+        chassis_id = json_data["chassis-inventory"][0]["chassis"][0]["serial-number"][0]["data"]
         for module in chassis_modules:
             if 'serial-number' in module:
                 hardware_writer.writerow(
                     [
                         module['name'][0]['data'],
                         module['description'][0]['data'],
-                        module['serial-number'][0]['data']
+                        module['serial-number'][0]['data'],
+                        (chassis_id)
                     ]
                 )
             if 'chassis-sub-module' in module:
@@ -37,7 +46,8 @@ def dump_json_to_csv(json_data):
                             [
                                 sub_module['name'][0]['data'],
                                 sub_module['description'][0]['data'],
-                                sub_module['serial-number'][0]['data']
+                                sub_module['serial-number'][0]['data'],
+                                (chassis_id)
                             ]
                         )
                     elif 'serial-number' in sub_module:
@@ -45,7 +55,8 @@ def dump_json_to_csv(json_data):
                             [
                                 sub_module['name'][0]['data'],
                                 "No Description",
-                                sub_module['serial-number'][0]['data']
+                                sub_module['serial-number'][0]['data'],
+                                (chassis_id)
                             ]
                         )
                     if 'chassis-sub-sub-module' in sub_module:
@@ -55,7 +66,8 @@ def dump_json_to_csv(json_data):
                                     [
                                         sub_sub_module['name'][0]['data'],
                                         sub_sub_module['description'][0]['data'],
-                                        sub_sub_module['serial-number'][0]['data']
+                                        sub_sub_module['serial-number'][0]['data'],
+                                        (chassis_id)
                                     ]
                                 )
                             elif 'serial-number' in sub_sub_module:
@@ -63,7 +75,8 @@ def dump_json_to_csv(json_data):
                                     [
                                         sub_sub_module['name'][0]['data'],
                                         "No Description",
-                                        sub_sub_module['serial-number'][0]['data']
+                                        sub_sub_module['serial-number'][0]['data'],
+                                        (chassis_id)
                                     ]
                                 )
                         
@@ -73,12 +86,15 @@ def dump_json_to_csv(json_data):
 if __name__ == "__main__":
     list_of_raw_files = []
     source_dir = "./chassis_hardware_outputs/"
-    for file in os.listdir(source_dir):
-        if file.endswith(".json"):
-            list_of_raw_files.append(file)
-    for file in list_of_raw_files:
-        full_file_path = "{0}/{1}".format(source_dir,file)
-        with open(full_file_path, "r") as data_file:
-            read_data = data_file.read()
-            json_data = output_to_json(read_data)
-        dump_json_to_csv(json_data)
+    if os.path.isdir(source_dir):
+        for file in os.listdir(source_dir):
+            if file.endswith(".json"):
+                list_of_raw_files.append(file)
+        for file in list_of_raw_files:
+            full_file_path = "{0}/{1}".format(source_dir,file)
+            with open(full_file_path, "r") as data_file:
+                read_data = data_file.read()
+                json_data = output_to_json(read_data)
+            dump_json_to_csv(json_data)
+    else:
+        print ("\n\n" + (source_dir) + " does not exist.\nPlease place your json files in " + (source_dir) + " and run again\n\n")
